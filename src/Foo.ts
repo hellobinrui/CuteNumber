1c85026fe82ea4f69b933bdcfaa97f83b71d38a5
binrui <bmu2@slb.com>
2022-10-25 17:10:04 +0800

    feat: add test and function

--- src/Foo.ts
+++ src/Foo.ts
@@ -2,4 +2,25 @@ export class Foo {
   bar() {
     return 'bar'
   }
+  
+}
+
+export function isCuteNumberGreaterThan10(numbers: number[]): boolean {
+  const Evens = evenNumbers(numbers);
+  const TheMax = maxNumbers(Evens);
+  console.log(TheMax);
+  const CuteNumber = isGreateThan10(TheMax);
+  console.log(CuteNumber);
+  return CuteNumber;
+ // return isGreateThan10(maxNumbers(evenNumbers(numbers)));
+}
+export function evenNumbers(numbers: number[]) : number[]{
+  return numbers.filter(number => number % 2 ==0);
+}
+
+export function maxNumbers(numbers: number[]): number {
+  return Math.max(...numbers);
+}
+export function isGreateThan10(arg: number): boolean {
+  return arg > 10;
 }
--- test/Foo.test.ts
+++ test/Foo.test.ts
@@ -1,7 +1,21 @@
 import {Foo} from '../src/Foo'
-
+import {isCuteNumberGreaterThan10} from "../src/Foo"
 describe('Foo', () => {
   it('bar', () => {
     expect(new Foo().bar()).toEqual('bar')
   })
 })
+it('should return cute number given numbers is greater than 10', () => {
+  // given numbers
+  const numbers = [4, 3, 2, 7, 8, 9, 15, 16, 18, 20];
+  // when
+  const actual: boolean = isCuteNumberGreaterThan10(numbers);
+
+  // then
+  expect(actual).toBeTruthy();
+
+
+});
+
+
+
